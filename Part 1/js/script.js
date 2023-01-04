4ca81916cf3cb07e8c1f5760f0bacc9100e89b99
Artem <markelovartemka@icloud.com>
2023-01-04 09:09:55 -0700

    first commit

---
+++ Part 1/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log("arr" + " - object");
+
+let incr = 10,
+    dcr = 10;
+
+// ++incr;
+// --dcr;
+
+console.log(++incr);
+console.log(--dcr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != 8);
+
+const isChecked = false,
+      isClose = true;
+
+console.log(!isChecked);
