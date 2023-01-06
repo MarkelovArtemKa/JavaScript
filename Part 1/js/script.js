55e17d94786eb8d1b8af79fe38f98d222fe3441c
Artem <markelovartemka@icloud.com>
2023-01-06 09:58:35 -0700

    Added cycles and conditions

--- Part 1/js/script.js
+++ Part 1/js/script.js
@@ -10,12 +10,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Last wacthed movie?", ""),
-      b = prompt("Grade it", ""),
-      c = prompt("Last wacthed movie?", ""),
-      d = prompt("Grade it", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Last wacthed movie?", ""),
+    b = prompt("Grade it", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != "" && b != "" && a.length < 50 ) {
+        personalMovieDB.movies[a] = b;
+        console.log("Success");
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count >= 0 && personalMovieDB.count < 10) {
+    console.log("Watched not many movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("You are clasical movie watcher!");
+} else if (personalMovieDB.count > 30){
+    console.log("You love movies!");
+} else {
+    console.log("Error! Try again.");
+}
 
 console.log(personalMovieDB);
